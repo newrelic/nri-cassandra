7fc231ee7a7b7d28ba7c00f66bdea5d6a59b79a2
cristianciutea <cristi.ciutea@gmail.com>
2019-02-13 15:22:27 +0100

    cleanup tests

--- tests/integration/cassandra_test.go
+++ tests/integration/cassandra_test.go
@@ -1,16 +1,16 @@
-package main
+// +build integration
+
+package integration
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
 	"github.com/newrelic/infra-integrations-sdk/log"
 	"github.com/newrelic/nri-cassandra/tests/integration/helpers"
 	"github.com/newrelic/nri-cassandra/tests/integration/jsonschema"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"regexp"
 	"strings"
@@ -90,17 +90,15 @@ func TestCassandraIntegration_ValidArguments(t *testing.T) {
 		fmt.Sprintf("NRIA_CACHE_PATH=/tmp/%v.json", testName),
 	)
 
-	require.NoError(t, err, "It isn't possible to execute Cassandra integration binary.")
+	assert.NoError(t, err, "It isn't possible to execute Cassandra integration binary.")
 
-	if stderr != "" {
-		t.Fatalf("Unexpected stderr output: %s", stderr)
-	}
+	assert.NotNil(t, stderr, "unexpected stderr")
 
 	schemaPath := filepath.Join(schemaFolder, "cassandra-schema.json")
 
 	err = jsonschema.Validate(schemaPath, stdout)
 
-	require.NoError(t, err, "The output of Cassandra integration doesn't have expected format.")
+	assert.NoError(t, err, "The output of Cassandra integration doesn't have expected format.")
 }
 
 func TestCassandraIntegration_OnlyMetrics(t *testing.T) {
@@ -112,16 +110,14 @@ func TestCassandraIntegration_OnlyMetrics(t *testing.T) {
 		fmt.Sprintf("NRIA_CACHE_PATH=/tmp/%v.json", testName),
 	)
 
-	require.NoError(t, err, "It isn't possible to execute Cassandra integration binary.")
+	assert.NoError(t, err, "It isn't possible to execute Cassandra integration binary.")
 
-	if stderr != "" {
-		t.Fatalf("Unexpected stderr output: %s", stderr)
-	}
+	assert.NotNil(t, stderr, "unexpected stderr")
 
 	schemaPath := filepath.Join(schemaFolder, "cassandra-schema-metrics.json")
 
 	err = jsonschema.Validate(schemaPath, stdout)
-	require.NoError(t, err, "The output of Cassandra integration doesn't have expected format.")
+	assert.NoError(t, err, "The output of Cassandra integration doesn't have expected format.")
 }
 
 func TestCassandraIntegration_OnlyInventory(t *testing.T) {
@@ -132,41 +128,31 @@ func TestCassandraIntegration_OnlyInventory(t *testing.T) {
 		fmt.Sprintf("NRIA_CACHE_PATH=/tmp/%v.json", testName),
 	)
 
-	require.NoError(t, err, "It isn't possible to execute Cassandra integration binary.")
+	assert.NoError(t, err, "It isn't possible to execute Cassandra integration binary.")
 
-	if stderr != "" {
-		t.Fatalf("Unexpected stderr output: %s", stderr)
-	}
+	assert.NotNil(t, stderr, "unexpected stderr")
 
 	schemaPath := filepath.Join(schemaFolder, "cassandra-schema-inventory.json")
 
 	err = jsonschema.Validate(schemaPath, stdout)
-	require.NoError(t, err, "The output of Cassandra integration doesn't have expected format.")
+	assert.NoError(t, err, "The output of Cassandra integration doesn't have expected format.")
 }
 
 func TestCassandraIntegration_Error_NoUserCredentials(t *testing.T) {
 	t.Skip("Skipping test - not correct message return - it will be fixed in JIRA ticket IHOST-176")
 	testName := helpers.GetTestName(t)
-	cmd := exec.Command(*(binPath))
 
-	cmd.Env = []string{
+	stdout, stderr, err := runIntegration(t,
 		fmt.Sprintf("NRIA_CACHE_PATH=%v", testName),
-	}
+	)
+
 	expectedErrorMessage := "Access denied"
-	var outbuf, errbuf bytes.Buffer
-	cmd.Stdout = &outbuf
-	cmd.Stderr = &errbuf
-	err := cmd.Run()
-	if err == nil {
-		t.Fatal("Error not returned")
-	}
-	errMatch, _ := regexp.MatchString(expectedErrorMessage, errbuf.String())
-	if !errMatch {
-		t.Fatalf("Expected error message: '%s', got: '%s'", expectedErrorMessage, errbuf.String())
-	}
-	if outbuf.String() != "" {
-		t.Fatalf("Unexpected output: %s", outbuf.String())
-	}
+
+	errMatch, _ := regexp.MatchString(expectedErrorMessage, stderr)
+	assert.Error(t, err, "Expected error")
+	assert.Truef(t, errMatch, "Expected error message: '%s', got: '%s'", expectedErrorMessage, stderr)
+
+	assert.NotNil(t, stdout, "unexpected stdout")
 }
 
 func TestCassandraIntegration_NoError_NoUserCredentials_OnlyInventory(t *testing.T) {
@@ -178,16 +164,14 @@ func TestCassandraIntegration_NoError_NoUserCredentials_OnlyInventory(t *testing
 		fmt.Sprintf("NRIA_CACHE_PATH=/tmp/%v.json", testName),
 	)
 
-	require.NoError(t, err, "It isn't possible to execute Cassandra integration binary.")
+	assert.NoError(t, err, "It isn't possible to execute Cassandra integration binary.")
 
-	if stderr != "" {
-		t.Fatalf("Unexpected stderr output: %s", stderr)
-	}
+	assert.NotNil(t, stderr, "unexpected stderr")
 
 	schemaPath := filepath.Join(schemaFolder, "cassandra-schema-inventory.json")
 
 	err = jsonschema.Validate(schemaPath, stdout)
-	require.NoError(t, err, "The output of Cassandra integration doesn't have expected format.")
+	assert.NoError(t, err, "The output of Cassandra integration doesn't have expected format.")
 }
 
 func TestCassandraIntegration_Error_NoPassword(t *testing.T) {
@@ -201,120 +185,84 @@ func TestCassandraIntegration_Error_NoPassword(t *testing.T) {
 	expectedErrorMessage := "Access denied"
 
 	errMatch, _ := regexp.MatchString(expectedErrorMessage, stderr)
-	if err == nil || !errMatch {
-		t.Fatalf("Expected error message: '%s', got: '%s'", expectedErrorMessage, stderr)
-	}
-	if stdout != "" {
-		t.Fatalf("Unexpected output: %s", stdout)
-	}
+	assert.Error(t, err, "Expected error")
+	assert.Truef(t, errMatch, "Expected error message: '%s', got: '%s'", expectedErrorMessage, stderr)
+
+	assert.NotNil(t, stdout, "unexpected stdout")
 }
 
 func TestCassandraIntegration_Error_InvalidPassword(t *testing.T) {
 	t.Skip("Skipping test - not correct message return - it will be fixed in JIRA ticket IHOST-176")
 	testName := helpers.GetTestName(t)
-	cmd := exec.Command(*binPath)
 
-	cmd.Env = []string{
+	stdout, stderr, err := runIntegration(t,
 		"USERNAME=monitorRole",
 		"PASSWORD=invalid_password",
 		fmt.Sprintf("NRIA_CACHE_PATH=%v", testName),
-	}
+	)
+
 	expectedErrorMessage := "Access denied"
-	var outbuf, errbuf bytes.Buffer
-	cmd.Stdout = &outbuf
-	cmd.Stderr = &errbuf
-	err := cmd.Run()
-	if err == nil {
-		t.Fatal("Error not returned")
-	}
-	errMatch, _ := regexp.MatchString(expectedErrorMessage, errbuf.String())
-	if !errMatch {
-		t.Fatalf("Expected error message: '%s', got: '%s'", expectedErrorMessage, errbuf.String())
-	}
-	if outbuf.String() != "" {
-		t.Fatalf("Unexpected output: %s", outbuf.String())
-	}
+
+	errMatch, _ := regexp.MatchString(expectedErrorMessage, stderr)
+	assert.Error(t, err, "Expected error")
+	assert.Truef(t, errMatch, "Expected error message: '%s', got: '%s'", expectedErrorMessage, stderr)
+
+	assert.NotNil(t, stdout, "unexpected stdout")
 }
 
 func TestCassandraIntegration_Error_InvalidUsername(t *testing.T) {
 	t.Skip("Skipping test - not correct message return - it will be fixed in JIRA ticket IHOST-176")
 	testName := helpers.GetTestName(t)
-	cmd := exec.Command(*binPath)
 
-	cmd.Env = []string{
+	stdout, stderr, err := runIntegration(t,
 		"USERNAME=invalid_username",
 		"PASSWORD=monitorPwd",
 		fmt.Sprintf("NRIA_CACHE_PATH=%v", testName),
-	}
+	)
+
 	expectedErrorMessage := "Access denied"
-	var outbuf, errbuf bytes.Buffer
-	cmd.Stdout = &outbuf
-	cmd.Stderr = &errbuf
-	err := cmd.Run()
-	if err == nil {
-		t.Fatal("Error not returned")
-	}
-	errMatch, _ := regexp.MatchString(expectedErrorMessage, errbuf.String())
-	if !errMatch {
-		t.Fatalf("Expected error message: '%s', got: '%s'", expectedErrorMessage, errbuf.String())
-	}
-	if outbuf.String() != "" {
-		t.Fatalf("Unexpected output: %s", outbuf.String())
-	}
+
+	errMatch, _ := regexp.MatchString(expectedErrorMessage, stderr)
+	assert.Error(t, err, "Expected error")
+	assert.Truef(t, errMatch, "Expected error message: '%s', got: '%s'", expectedErrorMessage, stderr)
+
+	assert.NotNil(t, stdout, "unexpected stdout")
 }
 
 func TestCassandraIntegration_Error_InvalidHostname(t *testing.T) {
 	t.Skip("Skipping test - not correct message return - it will be fixed in JIRA ticket IHOST-176")
 	testName := helpers.GetTestName(t)
-	cmd := exec.Command(*binPath)
-
-	cmd.Env = []string{
+	stdout, stderr, err := runIntegration(t,
 		"HOSTNAME=nonExistingHost",
 		fmt.Sprintf("NRIA_CACHE_PATH=%v", testName),
-	}
+	)
+
 	expectedErrorMessage := "no such host"
-	var outbuf, errbuf bytes.Buffer
-	cmd.Stdout = &outbuf
-	cmd.Stderr = &errbuf
-	err := cmd.Run()
-	if err == nil {
-		t.Fatal("Error not returned")
-	}
-	errMatch, _ := regexp.MatchString(expectedErrorMessage, errbuf.String())
-	if !errMatch {
-		t.Fatalf("Expected error message: '%s', got: '%s'", expectedErrorMessage, errbuf.String())
-	}
-	if outbuf.String() != "" {
-		t.Fatalf("Unexpected output: %s", outbuf.String())
-	}
+
+	errMatch, _ := regexp.MatchString(expectedErrorMessage, stderr)
+	assert.Error(t, err, "Expected error")
+	assert.Truef(t, errMatch, "Expected error message: '%s', got: '%s'", expectedErrorMessage, stderr)
+
+	assert.NotNil(t, stdout, "unexpected stdout")
 }
 
 func TestCassandraIntegration_Error_InvalidPort(t *testing.T) {
 	t.Skip("Skipping test - not correct message return - it will be fixed in JIRA ticket IHOST-176")
 	testName := helpers.GetTestName(t)
-	cmd := exec.Command(*binPath)
-
-	cmd.Env = []string{
+	stdout, stderr, err := runIntegration(t,
 		"USERNAME=monitorRole",
 		"PASSWORD=monitorPwd",
 		"PORT=1",
 		fmt.Sprintf("NRIA_CACHE_PATH=%v", testName),
-	}
+	)
+
 	expectedErrorMessage := "Failed to connect, invalid port"
-	var outbuf, errbuf bytes.Buffer
-	cmd.Stdout = &outbuf
-	cmd.Stderr = &errbuf
-	err := cmd.Run()
-	if err == nil {
-		t.Fatal("Error not returned")
-	}
-	errMatch, _ := regexp.MatchString(expectedErrorMessage, errbuf.String())
-	if !errMatch {
-		t.Fatalf("Expected error message: '%s', got: '%s'", expectedErrorMessage, errbuf.String())
-	}
-	if outbuf.String() != "" {
-		t.Fatalf("Unexpected output: %s", outbuf.String())
-	}
+
+	errMatch, _ := regexp.MatchString(expectedErrorMessage, stderr)
+	assert.Error(t, err, "Expected error")
+	assert.Truef(t, errMatch, "Expected error message: '%s', got: '%s'", expectedErrorMessage, stderr)
+
+	assert.NotNil(t, stdout, "unexpected stdout")
 }
 
 func TestCassandraIntegration_Error_InvalidConfigPath_NonExistingFile(t *testing.T) {
@@ -329,13 +277,10 @@ func TestCassandraIntegration_Error_InvalidConfigPath_NonExistingFile(t *testing
 	expectedErrorMessage := "no such file or directory"
 
 	errMatch, _ := regexp.MatchString(expectedErrorMessage, stderr)
-	if err == nil || !errMatch {
-		t.Fatalf("Expected error message: '%s', got: '%s'", expectedErrorMessage, stderr)
-	}
+	assert.Error(t, err, "Expected error")
+	assert.Truef(t, errMatch, "Expected error message: '%s', got: '%s'", expectedErrorMessage, stderr)
 
-	if stdout != "" {
-		t.Fatalf("Unexpected output: %s", stdout)
-	}
+	assert.NotNil(t, stdout, "unexpected stdout")
 }
 
 func TestCassandraIntegration_NoError_InvalidConfigPath_NonExistingFile_OnlyMetrics(t *testing.T) {
@@ -350,20 +295,18 @@ func TestCassandraIntegration_NoError_InvalidConfigPath_NonExistingFile_OnlyMetr
 		fmt.Sprintf("NRIA_CACHE_PATH=/tmp/%v.json", testName),
 	)
 
-	require.NoError(t, err, "It isn't possible to execute Cassandra integration binary.")
+	assert.NoError(t, err, "It isn't possible to execute Cassandra integration binary.")
 
-	if stderr != "" {
-		t.Fatalf("Unexpected stderr output: %s", stderr)
-	}
+	assert.NotNil(t, stderr, "unexpected stderr")
 
 	// Cassandra 3.7 does not report DataCenter and Racks names through JMX
 	schemaPath := filepath.Join(schemaFolder, "cassandra-schema-metrics.json")
 
 	err = jsonschema.Validate(schemaPath, stdout)
-	require.NoError(t, err, "The output of Cassandra integration doesn't have expected format.")
+	assert.NoError(t, err, "The output of Cassandra integration doesn't have expected format.")
 
 	err = jsonschema.Validate(schemaPath, stdout)
-	require.NoError(t, err, "The output of Cassandra integration doesn't have expected format.")
+	assert.NoError(t, err, "The output of Cassandra integration doesn't have expected format.")
 }
 
 func TestCassandraIntegration_Error_InvalidConfigPath_ExistingFile(t *testing.T) {
@@ -375,33 +318,23 @@ func TestCassandraIntegration_Error_InvalidConfigPath_ExistingFile(t *testing.T)
 	defer os.Remove(tmpfile.Name())
 
 	testName := helpers.GetTestName(t)
-	cmd := exec.Command(*binPath)
-	cmd.Env = []string{
+	stdout, stderr, err := runIntegration(t,
 		fmt.Sprintf("CONFIG_PATH=%s", tmpfile.Name()),
 		"INVENTORY=true",
 		fmt.Sprintf("NRIA_CACHE_PATH=%v", testName),
-	}
+	)
+
+	assert.NoError(t, err, "It isn't possible to execute Cassandra integration binary.")
 
 	expectedErrorMessage := "Config path not correctly set, cannot fetch inventory data"
-	var outbuf, errbuf bytes.Buffer
-	cmd.Stdout = &outbuf
-	cmd.Stderr = &errbuf
-	err = cmd.Run()
-	if err != nil {
-		t.Fatalf("It isn't possible to execute Cassandra integration binary. Err: %s -- %s", err, errbuf.String())
-	}
 
 	schemaPath := filepath.Join(schemaFolder, "cassandra-schema-empty.json")
 
-	err = jsonschema.Validate(schemaPath, outbuf.String())
-	if err != nil {
-		t.Fatalf("The output of Cassandra integration doesn't have expected format. Err: %s", err)
-	}
+	err = jsonschema.Validate(schemaPath, stdout)
+	assert.NoError(t, err, "The output of Cassandra integration doesn't have expected format.")
 
-	errMatch, _ := regexp.MatchString(expectedErrorMessage, errbuf.String())
-	if !errMatch {
-		t.Fatalf("Expected warning message: '%s', got: '%s'", expectedErrorMessage, errbuf.String())
-	}
+	errMatch, _ := regexp.MatchString(expectedErrorMessage, stderr)
+	assert.Truef(t, errMatch, "Expected error message: '%s', got: '%s'", expectedErrorMessage, stderr)
 }
 
 func TestCassandraIntegration_Error_InvalidConfigPath_ExistingDirectory(t *testing.T) {
@@ -416,11 +349,8 @@ func TestCassandraIntegration_Error_InvalidConfigPath_ExistingDirectory(t *testi
 	expectedErrorMessage := "is a directory"
 
 	errMatch, _ := regexp.MatchString(expectedErrorMessage, stderr)
-	if err == nil || !errMatch {
-		t.Fatalf("Expected error message: '%s', got: '%s'", expectedErrorMessage, stderr)
-	}
+	assert.Error(t, err, "Expected error")
+	assert.Truef(t, errMatch, "Expected error message: '%s', got: '%s'", expectedErrorMessage, stderr)
 
-	if stdout != "" {
-		t.Fatalf("Unexpected output: %s", stdout)
-	}
+	assert.NotNil(t, stdout, "unexpected stdout")
 }
